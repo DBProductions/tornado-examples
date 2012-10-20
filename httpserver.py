f5b9f0ed3d3a44ee7daa249bc0139e5769f3359c
DBProductions <dennis@dbproductions.de>
2012-10-20 17:45:21 +0200

    add basic examples

---
+++ httpserver.py
@@ -0,0 +1,11 @@
+import tornado.httpserver
+import tornado.ioloop
+
+def handle_request(request):
+    message = "You requested %s\n" % request.uri
+    request.write("HTTP/1.1 200 OK\r\nContent-Length: %d\r\n\r\n%s" % (len(message), message))
+    request.finish()
+
+http_server = tornado.httpserver.HTTPServer(handle_request)
+http_server.listen(8888)
+tornado.ioloop.IOLoop.instance().start()
---
+++ requesthandler.py
@@ -0,0 +1,13 @@
+import tornado.ioloop
+import tornado.web
+
+class MainHandler(tornado.web.RequestHandler):        
+    def get(self):
+        message = "You requested %s\n" % self.request.uri
+        self.write(message)
+
+application = tornado.web.Application([(r".*", MainHandler)])
+
+if __name__ == "__main__":
+    application.listen(8888)
+    tornado.ioloop.IOLoop.instance().start()
