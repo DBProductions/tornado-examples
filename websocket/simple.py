c86ec6ce59644d9cf93b344abd6f4b373a5017d3
DBProductions <dennis@dbproductions.de>
2015-07-28 20:47:23 +0200

    implement a broadcast method into SocketHandler

--- websocket/simple.py
+++ websocket/simple.py
@@ -6,15 +6,22 @@ import tornado.ioloop
 import tornado.websocket
 import tornado.httpserver
 
+SOCKET_CLIENTS = []
+
 class SocketHandler(tornado.websocket.WebSocketHandler):
+    """ Handle the incoming sockets """
     def open(self):
         self.socket_id = str(uuid.uuid4())
-        print "WebSocket opened", self.socket_id
+        SOCKET_CLIENTS.append(self)
     def on_message(self, message):
-        print self.socket_id, "send message"
-        self.write_message(message)
+        self.write_message(message)        
+    def broadcast(self, message, this_not=None):
+        for c in SOCKET_CLIENTS:
+            if c is not this_not:
+                c.write_message(message)
     def on_close(self):
-        print "WebSocket closed"
+        self.broadcast('socket disconnected', self)
+        SOCKET_CLIENTS.remove(self)
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
